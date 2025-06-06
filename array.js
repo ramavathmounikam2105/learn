cff338aae5ffeaf19c0e49aaf8ce00978cb0859a
ramavathmounikam <ramavathmounika2105@gmail.com>
2025-06-06 15:29:40 +0000

    hii

---
+++ array.js
@@ -0,0 +1,14 @@
+let fruits=["Apple","Mango","orange"]
+// console.log(fruits)
+// console.log(typeof fruits)
+// console.log(fruits[1])
+// console.log(fruits)
+// fruits[1]="cherry"
+// console.log(fruits)
+
+// fruits.push("Tomato") //.push is a method which addss array element at last of the array
+// console.log(fruits)
+// fruits.shift()// shift is a method which removes the element from the first
+// fruits.pop()//.pop is a method which rremoves element from the last
+ fruits.unshift("papaya")// unshift is a method which add at first index of the element of array
+ console.log(fruits)
---
+++ functions.js
@@ -0,0 +1,36 @@
+// setTimeout(
+// function sayBye() {
+//     console.log("Bye Hari")
+// }
+
+// ,3000)
+
+// sayHello()//calling the function
+
+// //declaring of function
+// function sayHello(){
+//     console.log("Hi")
+// }
+// sayHello()
+// sayHello()
+// sayHello()
+// sayHello()
+// sayHello()
+ 
+//what is Arrow Function
+//Arrow functions(<=)
+
+//
+
+// function sum(a,b) {
+//     console.log(a*b)
+// }
+
+// sum(10,5)
+
+// const sum =(a,b) => {
+//     console.log(a+b)
+// }
+// sum(10,5)
+
+//import export of functions
